195c7a226546137a15c90afb7ce35f317f699ea3
Demmyjsx <ademolaabds@gmail.com>
2025-11-06 23:08:20 +0100

    created other components

---
+++ src/app/Contact/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ContactHeader from '../Components/ContactHeader'
+import ContactForm from '../Components/ContactForm'
+import Footer from '../Components/Footer'
+
+export default function page() {
+  return (
+    <>
+      <ContactHeader />
+      <ContactForm />
+      <Footer />
+    </>
+  )
+}
