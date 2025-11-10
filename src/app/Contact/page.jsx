83f5e3a4ba1e5acd257b82984b48d0e03a9f9d12
Demmyjsx <ademolaabds@gmail.com>
2025-11-10 19:04:39 +0100

    corrected  some error

--- src/app/Contact/page.jsx
+++ src/app/Contact/page.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ContactHeader from '../Components/ContactHeader'
-import ContactForm from '../Components/ContactForm'
+
 import Footer from '../Components/Footer'
 
-export default function ServicePage() {
+export default function page() {
   return (
     <>
       <ContactHeader />
-      <ContactForm />
+      
       <Footer />
     </>
   )
